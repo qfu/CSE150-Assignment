6c72411d7b47c96b8f36be7127c6d210131fa2d5
Zhengkun Tan <z2tan@ucsd.edu>
2015-04-11 14:20:23 -0700

    assignment1 must try

--- assignment1_p1.py
+++ assignment1_p1.py
@@ -1,2 +1,19 @@
 #This is the first Program 
 #
+__author__ = 'z2tan@ucsd.edu,qfu@ucsd.edu'
+def is_complete(board):
+	# your code here
+	check = 0
+	for row in board:
+		for col in row:
+			if col != check:
+				return False
+			else:
+				check += 1	
+	return True
+def main():
+	import sys
+	board=[[int(n.strip()) for n in line.split(',')] for line in sys.stdin.readlines()]
+	print(is_complete(board))
+if __name__ == '__main__':
+	main()
